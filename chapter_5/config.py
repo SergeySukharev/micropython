3e086210d93d33c805ca7f3c5bc958a303e98417
Сухарев Сергей Юрьевич <sysukharev@mts.ru>
2021-10-10 12:14:07 +0300

    adding project files

---
+++ chapter_5/config.py
@@ -0,0 +1,10 @@
+DHT22_PIN = 4  # D2
+FAHRENHEIT = False
+WEBHOOK_URL = 'https://api.thingspeak.com/update?api_key=9SCEDQYIJ1MQMUUH&field1={temperature}&field2={humidity}'
+DEBUG_PIN = 14  # D5
+LOG_INTERVAL = 60
+
+WIFI_SSID = 'Party'
+WIFI_PASSWORD = '13091989'
+BUTTON_ID = 'micropython1'
+LED_PIN = 2  # D4
